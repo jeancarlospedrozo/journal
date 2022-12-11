703b5b321906604d0834bbf42f956f5c23b6eecf
Jean Carlos <jeancapedrozo@gmail.com>
2022-12-10 23:20:31 -0500

    Inicializando Redux y formik agregado

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,64 +1,90 @@
 import { Google } from "@mui/icons-material";
-import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Field, Form, Formik } from "formik";
 import React from "react";
+import { useDispatch } from "react-redux";
 import { Link as RouterLink } from "react-router-dom";
+import { checkingCredentials } from "../../store/auth/authSlice";
+import {
+  checkingAuthentication,
+  startGoogleSingIn,
+} from "../../store/auth/thunks";
 import { AuthLayout } from "../layout/AuthLayout";
 
 export const Login = () => {
+  const dispatch = useDispatch();
+
   return (
     <AuthLayout title="Login">
-      <Box component="form" noValidate sx={{ mt: 1 }}>
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
-      </Box>
-      <Grid container spacing={2} sx={{ mt: 1, mb: 2 }}>
-        <Grid item xs={6}>
-          <Button type="submit" fullWidth variant="contained">
-            Sign In
-          </Button>
-        </Grid>
-        <Grid item xs={6}>
-          <Button type="submit" fullWidth variant="contained">
-            <Google />
-            <Typography sx={{ ml: 1 }}>Google</Typography>
-          </Button>
-        </Grid>
-      </Grid>
-      <Grid container>
-        <Grid item xs>
-          <Link href="#" variant="body2">
-            Forgot password?
-          </Link>
-        </Grid>
-        <Grid item>
-          <Link
-            component={RouterLink}
-            to="/auth/register"
-            variant="body2"
-            underline="none"
-          >
-            {"Don't have an account? Sign Up"}
-          </Link>
-        </Grid>
-      </Grid>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        <Form>
+          <Field
+            as={TextField}
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <Field
+            as={TextField}
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Grid container spacing={2} sx={{ mt: 1, mb: 2 }}>
+            <Grid item xs={6}>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                onClick={() => dispatch(checkingAuthentication())}
+              >
+                Sign In
+              </Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                onClick={() => dispatch(startGoogleSingIn())}
+              >
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container>
+            <Grid item>
+              <Link
+                component={RouterLink}
+                to="/auth/register"
+                variant="body2"
+                underline="none"
+              >
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Form>
+      </Formik>
     </AuthLayout>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { store } from "./store/store";
 import { AppTheme } from "./theme/AppTheme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppTheme>
-        <App />
-      </AppTheme>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppTheme>
+          <App />
+        </AppTheme>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
