45832dbe03a85414f7e6105b2afc9a1487f3335b
Jean Carlos <jeancapedrozo@gmail.com>
2023-01-21 22:08:50 -0500

    Agregar nota firebase

--- src/journal/pages/Journal.jsx
+++ src/journal/pages/Journal.jsx
@@ -1,16 +1,25 @@
 import { AddOutlined } from "@mui/icons-material";
 import { IconButton, Typography } from "@mui/material";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { startNewNote } from "../../store/journal/thunks";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteViews, NothingSelectedView } from "../views";
 
 export const Journal = () => {
+  const dispatch = useDispatch();
+
+  const clickNewNote = () => {
+    dispatch(startNewNote());
+  };
+
   return (
     <JournalLayout>
       {/* <Typography variant={"h1"}>Journal</Typography> */}
       <NothingSelectedView />
       {/* <NoteViews /> */}
       <IconButton
+        onClick={clickNewNote}
         size="large"
         sx={{
           position: "fixed",
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,5 +1,23 @@
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { firebaseDB } from "../../firebase/config";
 import { addNewNote } from "./journalSlice";
 
-export const startNewNote = () => async (dispatch) => {
-  dispatch(addNewNote());
+export const startNewNote = () => {
+  return async (dispatch, getState) => {
+    // console.log("🚀 ~ file: thunks.js:7 ~ return ~ getState", getState());
+    const { uid } = getState().auth;
+
+    const newNote = {
+      title: "",
+      body: "",
+      date: new Date().getTime(),
+    };
+
+    const newDoc = doc(collection(firebaseDB, `${uid}/journal/notas`));
+    console.log("🚀 ~ file: thunks.js:9 ~ startNewNote ~ newDoc", newDoc);
+
+    await setDoc(newDoc, newNote);
+
+    // dispatch(addNewNote());
+  };
 };
